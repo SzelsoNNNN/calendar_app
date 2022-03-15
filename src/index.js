9f805815bbacf35bfbd75989f3e43bca8355f080
SzelsoNNNN <72217754+SzelsoNNNN@users.noreply.github.com>
2022-03-15 15:35:58 +0100

    static
    Statyczna strona, wszystko w jednym returnie

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import Static from './static/static'
 
 ReactDOM.render(
-  <App />,
+  <Static />,
   document.getElementById('root')
 )
